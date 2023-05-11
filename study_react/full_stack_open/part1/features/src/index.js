c6be7b3f79aa57ddf9117cc7968b8f400066c772
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-05-11 16:31:04 +0300

    Stateful component

--- study_react/full_stack_open/part1/features/src/index.js
+++ study_react/full_stack_open/part1/features/src/index.js
@@ -1,18 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from './App'
+import App from './App';
 
-let counter = 1
-
-const refresh = () => {
-    ReactDOM.createRoot(document.getElementById('root')).render(
-        <App counter={counter} />
-    )
-}
-
-refresh()
-counter += 1
-refresh()
-counter += 1
-refresh()
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
