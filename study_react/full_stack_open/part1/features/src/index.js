19bf07f60d9d8b0b21a431f3267b15f0c7872cdb
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-05-11 16:29:31 +0300

    Page re-rendering

--- study_react/full_stack_open/part1/features/src/index.js
+++ study_react/full_stack_open/part1/features/src/index.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import App from './App';
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+let counter = 1
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App counter={counter} />
+)
