3d31ec7f982d34d4548a6b766c1dee0323bbaa5e
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-05-11 16:34:37 +0300

    Event handling2

--- study_react/full_stack_open/part1/features/src/App.js
+++ study_react/full_stack_open/part1/features/src/App.js
@@ -3,14 +3,10 @@ import { useState } from 'react'
 const App = () => {
   const [counter, setCounter] = useState(0)
 
-  const handleClick = () => {
-    console.log('clicked')
-  }
-
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={handleClick}>
+      <button onClick={() => console.log('clicked')}>
         plus
       </button>
     </div>
