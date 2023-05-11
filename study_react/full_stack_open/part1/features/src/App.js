1ecda930123b026eb70d2351b5593ea878f47903
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-05-11 16:33:36 +0300

    Event handling

--- study_react/full_stack_open/part1/features/src/App.js
+++ study_react/full_stack_open/part1/features/src/App.js
@@ -3,15 +3,17 @@ import { useState } from 'react'
 const App = () => {
   const [counter, setCounter] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
-  console.log('rendering...', counter)
+  const handleClick = () => {
+    console.log('clicked')
+  }
 
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={handleClick}>
+        plus
+      </button>
+    </div>
   )
 }
 
