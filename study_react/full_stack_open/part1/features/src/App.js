5e680b6c1e8bf5e4100f198f1209808b5a34c31a
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-05-11 16:28:23 +0300

    Destructuring3

--- study_react/full_stack_open/part1/features/src/App.js
+++ study_react/full_stack_open/part1/features/src/App.js
@@ -1,10 +1,11 @@
-const Hello = (props) => {
-  const { name, age } = props
+const Hello = ({ name, age }) => {
   const bornYear = () => new Date().getFullYear() - age
 
   return (
     <div>
-      <p>Hello {name}, you are {age} years old</p>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
   )
