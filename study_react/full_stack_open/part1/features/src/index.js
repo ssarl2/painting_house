5d4bda445e156310520a7cde50516a2f0671400f
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-05-11 16:30:25 +0300

    Page re-rendering2

--- study_react/full_stack_open/part1/features/src/index.js
+++ study_react/full_stack_open/part1/features/src/index.js
@@ -5,6 +5,14 @@ import App from './App'
 
 let counter = 1
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter} />
-)
+const refresh = () => {
+    ReactDOM.createRoot(document.getElementById('root')).render(
+        <App counter={counter} />
+    )
+}
+
+refresh()
+counter += 1
+refresh()
+counter += 1
+refresh()
