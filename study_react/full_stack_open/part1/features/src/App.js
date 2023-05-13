e43c06e0f87370d71e69b1d62323ead4c809d919
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-05-13 13:49:55 +0300

    Conditional rendering2

--- study_react/full_stack_open/part1/features/src/App.js
+++ study_react/full_stack_open/part1/features/src/App.js
@@ -15,6 +15,12 @@ const History = (props) => {
   )
 }
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
@@ -38,8 +44,8 @@ const App = () => {
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
       {right}
       <History allClicks={allClicks} />
     </div>
