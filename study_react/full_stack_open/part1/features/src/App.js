f8c839d2f651a2244ce38b0c7e3c945094370186
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-05-13 13:28:55 +0300

    Update of the state is asynchronous

--- study_react/full_stack_open/part1/features/src/App.js
+++ study_react/full_stack_open/part1/features/src/App.js
@@ -4,15 +4,18 @@ const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
     setLeft(left + 1)
+    setTotal(left + right)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
     setRight(right + 1)
+    setTotal(left + right)
   }
 
   return (
@@ -22,6 +25,7 @@ const App = () => {
       <button onClick={handleRightClick}>right</button>
       {right}
       <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
     </div>
   )
 }
