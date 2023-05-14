d0f5fc84e0d91d00d4017f5c6284bb3aa6c0d3cf
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-05-14 16:17:25 +0300

    A function that returns a function

--- study_react/full_stack_open/part1/features/src/App.js
+++ study_react/full_stack_open/part1/features/src/App.js
@@ -3,15 +3,15 @@ import { useState } from 'react'
 const App = () => {
   const [value, setValue] = useState(10)
 
-  const handleClick = () => {
-    console.log('clicked the button')
-    setValue(0)
+  const hello = () => {
+    const handler = () => console.log('hello world')
+    return handler
   }
 
   return (
     <div>
       {value}
-      <button onClick={handleClick}>button</button>
+      <button onClick={hello()}>button</button>
     </div>
   )
 }
