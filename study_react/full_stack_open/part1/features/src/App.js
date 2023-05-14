9ee385918cae168155ef135719f7ba19c5a1b3cd
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-05-14 16:12:06 +0300

    Event Handling Revisited

--- study_react/full_stack_open/part1/features/src/App.js
+++ study_react/full_stack_open/part1/features/src/App.js
@@ -1,32 +1,12 @@
 import { useState } from 'react'
 
 const App = () => {
-  // these are ok
-  const [age, setAge] = useState(0)
-  const [name, setName] = useState('Juha Tauriainen')
-
-  if (age > 10) {
-    // this does not work!
-    const [foobar, setFoobar] = useState(null)
-  }
-
-  for (let i = 0; i < age; i++) {
-    // also this is not good
-    const [rightWay, setRightWay] = useState(false)
-  }
-
-  const notGood = () => {
-    // and this is also illegal
-    const [x, setX] = useState(-1000)
-  }
+  const [value, setValue] = useState(10)
 
   return (
     <div>
-      <p>age: {age}</p>
-      <p>name: {name}</p>
-      <p>foobar: {foobar}</p>
-      <p>rightWay: {rightWay}</p>
-      <p notGood={notGood}>x: {x}</p>
+      {value}
+      <button onClick={() => setValue(0)}>reset to zero</button>
     </div>
   )
 }
