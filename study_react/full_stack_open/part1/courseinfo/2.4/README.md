9f970cec13065e7e633e8ea8abce432ba4fcb532
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-05-30 22:58:17 +0300

    Submit exercise 2.4

---
+++ study_react/full_stack_open/part1/courseinfo/2.4/README.md
@@ -0,0 +1,6 @@
+## Submit exercise 2.4
+[2.4: course information step9](<https://fullstackopen.com/en/part2/rendering_a_collection_modules#:~:text=2.4%3A%20Course%20information%20step9>)
+
+## Run
+`npm install`  
+`npm start`  
---
+++ study_react/full_stack_open/part1/courseinfo/2.4/src/App.js
@@ -0,0 +1,95 @@
+const Header = ({ courseName }) => {
+  return (
+    <div>
+      <h1>{courseName}</h1>
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {
+        parts.map(part => (
+          <p key={part.id}>
+            {part.name} {part.exercises}
+          </p>
+        ))
+      }
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const Sum = ({ parts }) => {
+  const total = parts.reduce((s, p) => s + p.exercises, 0)
+  return <p>total of {total} exercises</p>
+}
+
+const App = () => {
+  const course = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        }, {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  return (
+    <div>
+      {
+        course.map(c => (
+          <div key={c.id} >
+            <Course course={c} />
+            <Sum parts={c.parts} />
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default App
