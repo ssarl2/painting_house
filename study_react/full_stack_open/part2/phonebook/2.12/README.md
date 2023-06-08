f53898dbf8a3a3d04f4688e73940d2e372395877
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-06-08 22:30:14 +0300

    Submit exercise 2.12

---
+++ study_react/full_stack_open/part2/phonebook/2.12/README.md
@@ -0,0 +1,9 @@
+## Submit exercise 2.12
+[2.12: The Phonebook Step7](<https://fullstackopen.com/en/part2/altering_data_in_server#:~:text=2.12%3A%20The%20Phonebook%20step7>)
+
+## Install
+`npm install --no-audit`  
+
+## Run
+`npm run server`  
+`npm start`  
---
+++ study_react/full_stack_open/part2/phonebook/2.12/src/App.js
@@ -0,0 +1,87 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const addr = '192.168.1.99'
+
+const PersonForm = (props) => {
+  return (
+    <form onSubmit={props.addPerson}>
+      <div>
+        name: <input value={props.newName} onChange={(event) => props.setNewName(event.target.value)} />
+      </div>
+      <div>
+        number: <input value={props.newNumber} onChange={(event) => props.setNewNumber(event.target.value)} />
+      </div>
+      <button type="submit">add</button>
+    </form>
+  )
+}
+
+const Filter = ({ filterWord, setFilterWord }) => {
+  return (
+    <div>
+      filter shown with <input value={filterWord} onChange={(event) => setFilterWord(event.target.value)} />
+    </div>
+  )
+}
+
+const Persons = ({ persons, filterWord }) => {
+  return (
+    <div>
+      {persons.filter(person => person.name.includes(filterWord)).map(person =>
+        <div key={person.name}>{person.name} {person.number}</div>
+      )}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterWord, setFilterWord] = useState('')
+
+  useEffect(() => {
+    axios
+      .get(`http://${addr}:3001/persons`)
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    const nameExists = persons.some(person => newName === person.name)
+
+    if (nameExists) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    axios
+      .post(`http://${addr}:3001/persons`, personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filterWord={filterWord} setFilterWord={setFilterWord} />
+      <h3>Add a new</h3>
+      <PersonForm addPerson={addPerson} newName={newName} newNumber={newNumber} setNewName={setNewName} setNewNumber={setNewNumber} />
+      <h3>Numbers</h3>
+      <Persons persons={persons} filterWord={filterWord} />
+    </div>
+  )
+}
+
+export default App
