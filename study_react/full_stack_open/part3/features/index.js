1cfc9882fd33958378ecb679276155dfaaecccaf
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-06-17 13:30:12 +0300

    backendtest

--- study_react/full_stack_open/part3/features/index.js
+++ study_react/full_stack_open/part3/features/index.js
@@ -1,98 +1,94 @@
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 const app = express()
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path  :', request.path)
-  console.log('Body  :', request.body)
-  console.log('---')
-  next()
-}
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
-app.use(requestLogger)
 
-let notes = [
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons = [
   {
     "id": 1,
-    "content": "HTML is easy",
-    "important": true
+    "name": "Arto Hellas",
+    "number": "040-123456"
   },
   {
     "id": 2,
-    "content": "Browser can execute only JavaScript",
-    "important": false
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
   },
   {
     "id": 3,
-    "content": "GET and POST are the most important methods of HTTP protocol",
-    "important": true
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
   }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+const generateId = () => {
+  return id = Math.floor(Math.random() * 1000)
+}
+
+app.get('/info', (request, response) => {
+  const date = new Date()
+  response.send(`<div>Phonebook has info for 2 people<br/><br/>${date}</div>`)
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
+  const person = persons.filter(p => p.id === id)
 
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  response.json(person)
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  persons = persons.filter(p => p.id !== id)
 
   response.status(204).end()
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-app.post('/api/notes', (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (!body.name || !body.number) {
     return response.status(400).json({
-      error: 'content missing'
+      error: 'Either name or number is missing'
     })
   }
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    id: generateId(),
+  if (persons.some(p => p.name === body.name)) {
+    return response.status(409).json({
+      error: 'name must be unique'
+    })
   }
 
-  notes = notes.concat(note)
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
 
-  response.json(note)
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
-app.use(unknownEndpoint)
-
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
