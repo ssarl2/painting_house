fb9a48833b943c1bf637e83d8aa892e1a3b07640
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-06-19 01:58:49 +0300

    Other operations

--- study_react/full_stack_open/part3/features/index.js
+++ study_react/full_stack_open/part3/features/index.js
@@ -12,35 +12,31 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
-
-let notes = [
-  {
-    "id": 1,
-    "content": "HTML is easy",
-    "important": true
-  },
-  {
-    "id": 2,
-    "content": "Browser can execute only JavaScript",
-    "important": false
-  },
-  {
-    "id": 3,
-    "content": "GET and POST are the most important methods of HTTP protocol",
-    "important": true
-  }
-]
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  Note.find({ important: true }).then(notes => {
+  Note.find({}).then(notes => {
     response.json(notes)
   })
 })
@@ -57,11 +53,27 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-  response.status(204).end()
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -83,21 +95,7 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
 app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  }
-
-  next(error)
-}
 
 // this has to be the last loaded middleware.
 app.use(errorHandler)
