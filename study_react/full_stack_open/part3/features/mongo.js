cbba28fa0faf9d65f145090cc90c49a16ae35c0a
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-06-18 20:58:01 +0300

    Fetching objects from the database2

--- study_react/full_stack_open/part3/features/mongo.js
+++ study_react/full_stack_open/part3/features/mongo.js
@@ -30,7 +30,7 @@ const Note = mongoose.model('Note', noteSchema)
 //     mongoose.connection.close()
 // })
 
-Note.find({}).then(result => {
+Note.find({ important: true }).then(result => {
     result.forEach(note => {
         console.log(note)
     })
