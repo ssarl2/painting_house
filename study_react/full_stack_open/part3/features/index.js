b6d658399e2ededdc8455c93c36f5c5723a63677
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-06-19 01:21:14 +0300

    Moving error handling into middleware

--- study_react/full_stack_open/part3/features/index.js
+++ study_react/full_stack_open/part3/features/index.js
@@ -4,6 +4,16 @@ const cors = require('cors')
 const app = express()
 const Note = require('./models/note')
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path  :', request.path)
@@ -49,7 +59,7 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => {
       if (note) {
@@ -58,10 +68,7 @@ app.get('/api/notes/:id', (request, response) => {
         response.status(404).end()
       }
     })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({ error: 'malformatted id' })
-    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -91,6 +98,8 @@ app.post('/api/notes', (request, response) => {
 })
 
 app.use(unknownEndpoint)
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
