1c0ab902d8b484475735f9a3fb27b696af51144a
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-07-04 23:08:56 +0300

    A basic idea of the write page

--- painting_house/frontend/src/components/WritePage.js
+++ painting_house/frontend/src/components/WritePage.js
@@ -1,7 +1,33 @@
-const WritePage = () =>{
+import { useState } from 'react'
+
+const WritePage = () => {
+    const [title, setTitle] = useState('')
+    const [category, setCategory] = useState('')
+    const [images, setImages] = useState([]) //TODO support to upload images
+    const [description, setDescription] = useState('')
+    const [tags, setTags] = useState([]) //TODO parse tags with ',' and ignore white spaces
+    const [author, setAuthor] = useState('') //TODO fetch author's info automatically
+
     return (
         <div>
-            Hello who wants to write
+            <form>
+                <div>
+                    title: <input value={title} onChange={(event) => setTitle(event.target.value)} />
+                </div>
+                <div>
+                    category: <input value={category} onChange={(event) => setCategory(event.target.value)} />
+                </div>
+                <div>
+                    images: <input value={images} onChange={(event) => setImages(event.target.value)} />
+                </div>
+                <div>
+                    description: <input value={description} onChange={(event) => setDescription(event.target.value)} />
+                </div>
+                <div>
+                    tags: <input value={tags} onChange={(event) => setTags(event.target.value)} />
+                </div>
+                <button type="submit">Post</button>
+            </form>
         </div>
     )
 }
