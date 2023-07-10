04bb425f3959bf626bba560163b22460bb072872
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-07-10 23:33:40 +0300

    Adjust the length of the description and tags div

--- painting_house/frontend/src/components/Post.js
+++ painting_house/frontend/src/components/Post.js
@@ -55,6 +55,27 @@ const Profile = () => {
 }
 
 const Post = ({ post, setPosts }) => {
+    const [descAndTags, setDescAndTags] = useState('')
+    const [isExpanded, setIsExpanded] = useState(false)
+    const descAndTagsMaxLength = 120 // Maximum number of characters to display initially
+
+    useEffect(() => {
+        const tags = post.tags.map(tag => `#${tag}`).join(' ')
+        const combineDescAndTags = post.description !== '' ? `${post.description}, ${tags}` : `${tags}`
+        setDescAndTags(combineDescAndTags)
+    }, [post.description, post.tags])
+
+    const toggleExpand = () => {
+        setIsExpanded(!isExpanded)
+    }
+
+    const renderDescription = () => {
+        if (isExpanded || descAndTags.length <= descAndTagsMaxLength) {
+            return descAndTags
+        } else {
+            return descAndTags.slice(0, descAndTagsMaxLength)
+        }
+    }
 
     const handleClick = (id, title) => {
         if (window.confirm(`Delete ${title}?`))
@@ -83,9 +104,11 @@ const Post = ({ post, setPosts }) => {
                                 <tbody>
                                     <tr>
                                         <td>
-                                            <div style={{ height: '30vw' }}>
-                                                {post.description}
-                                                <div>{post.tags.join(", ")}</div>
+                                            <div className='postDescriptionAndTagsBox'>
+                                                {renderDescription()}{!isExpanded && descAndTags.length > descAndTagsMaxLength && (<button style={{ color: 'gray', background: 'none', border: 'none' }} onClick={toggleExpand}>...more</button>)}
+                                                {isExpanded && descAndTags.length > descAndTagsMaxLength && (
+                                                    <button style={{ color: 'gray', background: 'none', border: 'none' }} onClick={toggleExpand}>...less</button>
+                                                )}
                                             </div>
                                         </td>
                                     </tr>
