4f9da0bd98e157b7c5139321e1718b5f481ccf5a
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-07-12 15:45:19 +0300

    Add a feature that cuts long text so that it fits in the container size

--- painting_house/frontend/src/components/PostDescriptionAndTags.js
+++ painting_house/frontend/src/components/PostDescriptionAndTags.js
@@ -2,14 +2,17 @@ import { useState, useEffect, useRef } from 'react'
 
 const PostDescriptionAndTags = ({ postDescription, postTags }) => {
     const [descAndTags, setDescAndTags] = useState('')
+    const [calcText, setCalcText] = useState('')
+    const [maxLength, setMaxLength] = useState(0)
+    const [isCalcDone, setIsCalcDone] = useState(false)
     const [isExpanded, setIsExpanded] = useState(false)
     const [visibleMore, setVisibleMore] = useState(false)
     const descAndTagsBoxRef = useRef(null)
-    const maxLength = 150
 
     useEffect(() => {
         const tags = postTags.map(tag => `#${tag}`).join(' ')
         setDescAndTags(`${postDescription} ${tags}`)
+        setCalcText(`${postDescription}`)
     }, [postDescription, postTags])
 
     useEffect(() => {
@@ -18,31 +21,54 @@ const PostDescriptionAndTags = ({ postDescription, postTags }) => {
         }
     }, [descAndTags])
 
+    useEffect(() => {
+        calcMaxLength()
+    }, [calcText])
+
     const toggleExpand = () => {
         setIsExpanded(!isExpanded)
     }
 
+    const calcMaxLength = () => {
+        const reCalcRate = 10
+        if (descAndTagsBoxRef.current.scrollHeight > descAndTagsBoxRef.current.offsetHeight) {
+            setCalcText(calcText.slice(0, calcText.length - reCalcRate))
+            setIsCalcDone(false)
+        }
+        else {
+            setMaxLength(calcText.length)
+            if (calcText.length !== 0)
+                setIsCalcDone(true)
+        }
+    }
+
     const trimText = () => {
+        const expendingButtonLength = 10
         if (isExpanded) {
             return descAndTags
         } else {
-            return descAndTags.slice(0, maxLength)
+            return descAndTags.slice(0, maxLength - expendingButtonLength)
         }
     }
 
     return (
         <div className='postDescriptionAndTagsBox' ref={descAndTagsBoxRef}>
-            {trimText()}
-            {!isExpanded && (
-                <button className={visibleMore ? 'postTextExpendingButton' : 'hidePostTextExpendingButton'} onClick={toggleExpand}>
-                    ...more
-                </button>
-            )}
-            {isExpanded && (
-                <button className='postTextExpendingButton' onClick={toggleExpand}>
-                    ...less
-                </button>
-            )}
+            {!isCalcDone && <div>{calcText}</div>}
+            {isCalcDone &&
+                <div>
+                    {trimText()}
+                    {!isExpanded && (
+                        <button className={visibleMore ? 'postTextExpendingButton' : 'hidePostTextExpendingButton'} onClick={toggleExpand}>
+                            ...more
+                        </button>
+                    )}
+                    {isExpanded && (
+                        <button className='postTextExpendingButton' onClick={toggleExpand}>
+                            ...less
+                        </button>
+                    )}
+                </div>
+            }
         </div>
     )
 }
